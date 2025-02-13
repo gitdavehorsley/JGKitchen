de9779ac4a854bde4999e36467288bbed5b46d3a
gitdavehorsley <44680821+gitdavehorsley@users.noreply.github.com>
2025-02-13 17:02:19 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
