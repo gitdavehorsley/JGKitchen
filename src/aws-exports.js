8fc9da7867e65beb4f17a4ad3f66001ad0d4f5ed
gitdavehorsley <44680821+gitdavehorsley@users.noreply.github.com>
2025-02-13 16:58:36 -0500

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+// src/aws-exports.js
+export default {
+    Auth: {
+        region: 'YOUR_AWS_REGION',
+        userPoolId: 'YOUR_USER_POOL_ID',
+        identityPoolId: 'YOUR_IDENTITY_POOL_ID',
+        userPoolWebClientId: 'YOUR_CLIENT_ID'
+    }
+};
